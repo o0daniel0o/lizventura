dc1824e7259024df6cfadb757b7ac23f9caec1a8
daniel el furioso <32757789+o0daniel0o@users.noreply.github.com>
2018-10-20 01:03:24 -0500

    APP

---
+++ app.js
@@ -0,0 +1,67 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Usuario : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+
+function login(){
+
+  var userEmail = document.getElementById("txtEmail").value;
+  var userPass = document.getElementById("txtPassword").value;
+  var uid = user.uid;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error :  " + errorMessage);
+
+    // ...
+  });
+
+}
+
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+  
+//======================== USUARIO FIN ================================================
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
